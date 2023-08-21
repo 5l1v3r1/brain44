afa7a69cfc6ed30a85ec56f924f0ddebc7e83fbb
schreera <81811318+schreera@users.noreply.github.com>
2023-08-21 10:13:56 +0300

    Update gen_hex_st.py

--- gen_hex_st.py
+++ gen_hex_st.py
@@ -1,14 +1,20 @@
 import itertools
+import sys
 
 hex_chars = '0123456789abcdef'
-start_chars = '1'
-max_password_length = 7
-max_total_length = 64
 
-for start_char in start_chars:
-    for length in range(1, max_password_length + 1):
-        passwords = itertools.product(hex_chars, repeat=length)
-        for password in passwords:
-            for shift in range(max_total_length - length + 1):
-                password_str = start_char * shift + ''.join(password) + start_char * (max_total_length - shift - length)
-                print(password_str)
+max_password_length = 7  # Максимальная длина пароля
+max_total_length = 64    # Максимальная общая длина строки
+
+if len(sys.argv) != 2:
+    print("Использование: python3 ваш_скрипт.py [начальный символ]")
+    sys.exit(1)
+
+start_char = sys.argv[1]
+
+for length in range(1, max_password_length + 1):
+    passwords = itertools.product(hex_chars, repeat=length)
+    for password in passwords:
+        for shift in range(max_total_length - length + 1):
+            password_str = start_char * shift + ''.join(password) + start_char * (max_total_length - shift - length)
+            print(password_str)
